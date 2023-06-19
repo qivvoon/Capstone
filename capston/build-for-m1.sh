17329433083ebe22c286e31c85f45616ed607ac4
Junhyeong Park <wnsgudqkr718@gmail.com>
2023-06-19 16:53:57 +0900

    Delete capston directory

--- capston/build-for-m1.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mvn clean package
-docker build -t stock/configserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/configserver-0.0.1-SNAPSHOT.jar ./configserver
-docker build -t stock/eurekaserver:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/eurekaserver-0.0.1-SNAPSHOT.jar ./eurekaserver
-docker build -t stock/scrape:0.0.1-SNAPSHOT --build-arg JAR_FILE=target/scrape-0.0.1-SNAPSHOT.jar ./scrape
